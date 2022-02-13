a3fdb0092e7b2895d7efcef313f4742e8bb9d440
p-le <phu.le.dev@gmail.com>
2022-02-13 07:30:33 +0000

    update template

--- templates.tf
+++ templates.tf
@@ -1,31 +1,29 @@
 resource "local_file" "backend_manifests" {
-  count = var.backend_config.is_enabled ? 1 : 0
-  content = templatefile("${path.module}/templates/backend.tpl.yaml", {
-    component  = "backend",
+  content = templatefile("${path.module}/templates/backend.yaml.tpl", {
+    component  = "todoapp-backend",
     region     = var.region,
     project_id = var.project_id
     registry   = google_artifact_registry_repository.todoapp.name
     image_name = "todoapp-backend"
-    image_tag  = var.backend_config.image_version
+    image_tag  = var.backend_config.image_tag
   })
   filename = "${path.module}/manifests/backend.yaml"
 }
 
 resource "local_file" "frontend_manifests" {
-  count = var.frontend_config.is_enabled ? 1 : 0
-  content = templatefile("${path.module}/templates/frontend.tpl.yaml", {
-    component  = "frontend",
+  content = templatefile("${path.module}/templates/frontend.yaml.tpl", {
+    component  = "todoapp-frontend",
     region     = var.region,
     project_id = var.project_id
     registry   = google_artifact_registry_repository.todoapp.name
     image_name = "todoapp-frontend"
-    image_tag  = var.frontend_config.image_version
+    image_tag  = var.frontend_config.image_tag
   })
   filename = "${path.module}/manifests/frontend.yaml"
 }
 
 resource "local_file" "provider_gke" {
   count = var.frontend_config.is_enabled ? 1 : 0
-  content = file("${path.module}/templates/provider_gke.tf")
+  content = file("${path.module}/templates/provider_gke.tf.tpl")
   filename = "${path.module}/provider_gke.tf"
 }
--- templates/frontend.yaml.tpl
+++ templates/frontend.yaml.tpl
@@ -1,20 +1,20 @@
 kind: Deployment
 apiVersion: apps/v1
 metadata:
-  name: ${component}-frontend
+  name: ${component}
 spec:
   replicas: 2
   selector:
     matchLabels:
-      app: ${component}-frontend
+      app: ${component}
   template:
     metadata:
       labels:
-        app: ${component}-frontend
+        app: ${component}
     spec:
       containers:
-        - name: ${component}-frontend
-          image: ${region}-docker.pkg.dev/${project_id}/${registry}}/${image_name}:${image_tag}
+        - name: ${component}
+          image: ${region}-docker.pkg.dev/${project_id}/${registry}/${image_name}:${image_tag}
           imagePullPolicy: Always
           ports:
             - containerPort: 80
