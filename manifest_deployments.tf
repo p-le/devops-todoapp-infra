18bcc1e57c89cb49630f10fae49649f64302ec70
p-le <phu.le.dev@gmail.com>
2022-02-13 09:10:30 +0000

    fix template manifest

--- manifest_deployments.tf
+++ manifest_deployments.tf
@@ -4,7 +4,7 @@ data "kubectl_path_documents" "manifests" {
 }
 
 resource "kubectl_manifest" "deployment_manifests" {
-  count = fileexists("${path.module}/provider_gke.tf") ? 1 : 0
+  count = fileexists("${path.module}/provider_gke.tf") ? length(data.kubectl_path_documents.manifests.documents) : 0
   depends_on = [
     google_container_node_pool.primary_nodes[0]
   ]
